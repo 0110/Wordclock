ebacc86f01a3ec254d787a674cee2b84c8a54335
Ollo <Ollo@host>
2020-12-07 20:02:01 +0100

    Upgraded esptool to python3

--- os/flash.sh
+++ os/flash.sh
@@ -4,7 +4,7 @@ if [ $# -ne 1 ]; then
  echo "One parameter required: the device of the serial interface"
  echo "$0 <device>"
  echo "e.g.:"
- echo "$0 ttyUSB0"
+ echo "$0 /dev/ttyUSB0"
  exit 1
 fi
 
@@ -14,8 +14,8 @@ DEVICE=$1
 
 # check the serial connection
 
-if [ ! -c /dev/$DEVICE ]; then
- echo "/dev/$DEVICE does not exist"
+if [ ! -c $DEVICE ]; then
+ echo "$DEVICE does not exist"
  exit 1
 fi
 
@@ -24,13 +24,12 @@ if [ ! -f esptool.py ]; then
  echo "esptool.py"
  exit 1
 fi
-
-./esptool.py --port /dev/$DEVICE $BAUD read_mac
+python3 esptool.py --port $DEVICE $BAUD read_mac
 
 if [ $? -ne 0 ]; then
  echo "Error reading the MAC -> set the device into the bootloader!"
  exit 1
 fi
 echo "Flashing the new"
-#./esptool.py --port /dev/$DEVICE $BAUD write_flash -fm dio 0x00000 nodemcu2.bin
-./esptool.py --port /dev/$DEVICE $BAUD write_flash -fm dio 0x00000 0x00000.bin 0x10000 0x10000.bin 0x3fc000 esp_init_data_default.bin 0x07e000 blank.bin 0x3fe000 blank.bin
+#python3 esptool.py --port $DEVICE $BAUD write_flash -fm dio 0x00000 nodemcu2.bin
+python3 esptool.py --port $DEVICE write_flash -fm dio 0x00000 0x00000.bin 0x10000 0x10000.bin 0x3fc000 esp_init_data_default.bin
