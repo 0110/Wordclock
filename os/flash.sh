0e8fc8864666f919682e3749b44cb70299ddec5b
Ollo <Ollo@host>
2021-01-31 16:13:44 +0100

    Flash script improved

--- os/flash.sh
+++ os/flash.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
 if [ $# -ne 1 ]; then
- echo "One parameter required: the device of the serial interface"
- echo "$0 <device>"
- echo "e.g.:"
- echo "$0 /dev/ttyUSB0"
- exit 1
-fi
-
-DEVICE=$1
-#BAUD="--baud 57600"
-#BAUD="--baud 921600"
-
+   DEVICE=$1
 # check the serial connection
-
 if [ ! -c $DEVICE ]; then
  echo "$DEVICE does not exist"
  exit 1
+fi
+
+else
+	print "Autodetect serial port"
 fi
 
 if [ ! -f esptool.py ]; then
@@ -24,11 +17,17 @@ if [ ! -f esptool.py ]; then
  echo "esptool.py"
  exit 1
 fi
-python3 esptool.py --port $DEVICE $BAUD read_mac
+
+CMD="python3 esptool.py "
+if [ $# -eq 1 ]; then
+CMD="python3 esptool.py --port $DEVICE "
+fi
+
+$CMD read_mac
 
 if [ $? -ne 0 ]; then
  echo "Error reading the MAC -> set the device into the bootloader!"
  exit 1
 fi
-echo "Flashing the new"
-python3 esptool.py --port $DEVICE write_flash -fm dio 0x00000 0x00000.bin 0x10000 0x10000.bin 0x3fc000 esp_init_data_default.bin
+echo "Flashing the new firmware"
+$CMD write_flash -fm dio 0x00000 0x00000.bin 0x10000 0x10000.bin 0x3fc000 esp_init_data_default.bin
