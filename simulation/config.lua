57de18b37cbec98025dd460e8d6708ad91a1ab38
Ollo <Ollo@host>
2021-11-01 11:53:34 +0100

    Less different colors

--- simulation/config.lua
+++ simulation/config.lua
@@ -1,12 +1,12 @@
 green2=200
-red=128
+red=200
 blue=200
 
 color=string.char(0, 0, blue)
 color1=string.char(red, 0, 0)
-color2=string.char(tonumber(red*0.8), 0, 0)
-color3=string.char(0, tonumber(green2*0.4), 0)
-color4=string.char(0,0 ,tonumber(blue*0.2))
+color2=string.char(tonumber(red*0.9), 0, 0)
+color3=string.char(tonumber(red*0.8), 0, 0)
+color4=string.char(tonumber(red*0.7), 0, 0)
 
 colorBg=string.char(0,0,0) -- black is the default background color
 sntpserverhostname="ptbtime1.ptb.de"
