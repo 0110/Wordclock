3b2554ee53aff5f6b237cffee84d09622e9bd134
Ollo <Ollo@host>
2021-12-04 13:07:29 +0100

    Lower brightness at start

--- init.lua
+++ init.lua
@@ -6,10 +6,10 @@ local MAXLEDS=110
 local counter1=0
 ws2812.write(string.char(0,0,0):rep(114))
 local bootledtimer = tmr.create()
-bootledtimer:register(100, tmr.ALARM_AUTO, function (t)
+bootledtimer:register(75, tmr.ALARM_AUTO, function (t)
     counter1=counter1+1
     spaceLeds = math.max(MAXLEDS - (counter1*2), 0)
-    ws2812.write(string.char(32,0,0):rep(counter1) .. string.char(0,0,0):rep(spaceLeds) .. string.char(0,0,16):rep(counter1))
+    ws2812.write(string.char(16,0,0):rep(counter1) .. string.char(0,0,0):rep(spaceLeds) .. string.char(0,0,8):rep(counter1))
     if ((counter1*2) > 114) then
         t:unregister()
     end
