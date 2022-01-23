31cf255426d687f6e04e7b02c50803b987bce3cd
Ollo <Ollo@host>
2022-01-23 15:14:04 +0100

    variable name improved

--- init.lua
+++ init.lua
@@ -6,12 +6,12 @@ local MAXLEDS=110
 local counter1=0
 ws2812.write(string.char(0,0,0):rep(114))
 local bootledtimer = tmr.create()
-bootledtimer:register(75, tmr.ALARM_AUTO, function (t)
+bootledtimer:register(75, tmr.ALARM_AUTO, function (timer)
     counter1=counter1+1
     spaceLeds = math.max(MAXLEDS - (counter1*2), 0)
     ws2812.write(string.char(16,0,0):rep(counter1) .. string.char(0,0,0):rep(spaceLeds) .. string.char(0,0,8):rep(counter1))
     if ((counter1*2) > 114) then
-        t:unregister()
+        timer:unregister()
     end
 end)
 bootledtimer:start()
