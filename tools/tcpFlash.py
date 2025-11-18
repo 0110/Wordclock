27b71d354cb31ad23244eb315d9972b29b6d47aa
Ollo <Ollo@host>
2025-11-18 21:12:02 +0100

    Upgrade to python 3

--- tools/tcpFlash.py
+++ tools/tcpFlash.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import argparse
 import socket
@@ -8,11 +8,11 @@ import time
 
 def sendRecv(s, message, answer):
     msg = message + "\n"
-    s.sendall(msg)
-    reply = s.recv(4096)
+    s.sendall(bytes(msg, 'utf-8'))
+    reply = s.recv(4096).decode("utf-8")
     i=1
     while ((not (answer in reply)) and (i < 10)):
-        reply += s.recv(4096)
+        reply += s.recv(4096).decode("utf-8")
         i = i + 1
     if answer not in reply:
         return False
@@ -21,102 +21,102 @@ def sendRecv(s, message, answer):
 
 def sendCmd(s, message, cleaningEnter=False):
     msg = message + "\n"
-    s.sendall(msg)
+    s.sendall(bytes(msg, 'utf-8'))
     time.sleep(0.050)
-    reply = s.recv(4096)
+    reply = s.recv(4096).decode("utf-8")
     i=1
     while ((not (">" in reply)) and (i < 10)):
         time.sleep((0.050) * i)
-        reply += s.recv(4096)
+        reply += s.recv(4096).decode("utf-8")
         i = i + 1
 
 #    print "Send\t" + message
 #    print "Got\t" + reply
     if (cleaningEnter):
-        s.sendall("\n")
+        s.sendall(bytes("\n", 'utf-8'))
     if "stdin:1:" in reply:
-       print "ERROR, received : " + reply
+       print ("ERROR, received : " + reply)
        return False
     elif ">" in reply:
         return True
     else:
-        print "ERROR, received : " + reply
+        print ("ERROR, received : " + reply)
         return False
 
 def main(nodeip, luafile, volatile=None, outfile=None):
     if ( not os.path.isfile(luafile) ):
-        print "The file " + luafile + " is not available"
+        print ("The file " + luafile + " is not available")
     else:
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect((nodeip, 23))
-	    time.sleep(0.050)
-            s.sendall("\n")
+            time.sleep(0.050)
+            s.sendall(bytes("\n", 'utf-8'))
             # Receive the hello Message of answer of the ESP
             if (not sendRecv(s, "\n", "Welcome to ") ):
-                print "Cannot connect to the ESP"
+                print ("Cannot connect to the ESP")
                 s.close()
                 sys.exit(2)
             
             # Read all lines from the welcome message
             i=0
-            reply = s.recv(4096)
+            reply = s.recv(4096).decode("utf-8")
             while ((reply is not None) and (not (">" in reply)) and (i < 100)):
-                reply = s.recv(4096)
+                reply = s.recv(4096).decode("utf-8")
                 i = i + 1
 
 
             # Communication tests
             if ( not sendRecv(s, "print(12345)", "12345") ):
-                print "NOT communicating with an ESP8266 running LUA (nodemcu) firmware"
+                print ("NOT communicating with an ESP8266 running LUA (nodemcu) firmware")
                 s.close()
                 sys.exit(3)
              
             if (volatile is None):
                 if (outfile is None):
-                    print "Flashing " + luafile
+                    print ("Flashing " + luafile)
                     outfile=luafile
                 else:
-                    print "Flashing " + luafile + " as " + outfile
+                    print ("Flashing " + luafile + " as " + outfile)
                 sendCmd(s, "w= file.writeline", True)
                 sendCmd(s, "file.open(\"temp.lua\",\"w+\");", True)
             else:
-                print "Executing " + luafile + " on nodemcu"
+                print ("Executing " + luafile + " on nodemcu")
 
             with open(luafile) as f:
                 contents = f.readlines()
                 i=1
                 for line in contents:
-                    print "\rSending " + str(i) + "/" + str(len(contents)) + " ...",
+                    print ("\rSending " + str(i) + "/" + str(len(contents)) + " ...")
                     sys.stdout.flush()
                     l = line.rstrip()
-		    if ( l.endswith("]") ):
-			l = l + " "
-			print "add a space at the end"
+                    if ( l.endswith("]") ):
+                        l = l + " "
+                        print ("add a space at the end")
 
                     if (volatile is None):
                         if (not sendCmd(s, "w([==[" + l + "]==]);")):
-                            print "Cannot write line " + str(i)
+                            print ("Cannot write line " + str(i))
                             s.close()
                             sys.exit(4)
-                    else:
-                        if (not sendCmd(s, l)):
-                            print "Cannot write line " + str(i)
-                            s.close()
-                            sys.exit(4)
-                    i=i+1
+                        else:
+                            if (not sendCmd(s, l)):
+                                print ("Cannot write line " + str(i))
+                                s.close()
+                                sys.exit(4)
+                        i=i+1
 
             if (volatile is None):
                 # Finished with updating the file in LUA
                 if (not sendCmd(s, "w([[" + "--EOF" + "]]);")):
-                    print "Cannot write line " + "-- EOF"
+                    print ("Cannot write line " + "-- EOF")
                 if (not sendCmd(s, "file.close();")):
-                    print "Cannot close the file"
+                    print ("Cannot close the file")
                     sys.exit(4)
 
                 sendCmd(s, "file.remove(\"" + outfile +"\");", True)
                 if (not sendCmd(s, "file.rename(\"temp.lua\",\""+ outfile + "\")")):
-                    print "Cannot move temporary file to " + outfile
+                    print ("Cannot move temporary file to " + outfile)
 
                 
                 # Check if the file exists:
@@ -131,8 +131,8 @@ def main(nodeip, luafile, volatile=None, outfile=None):
             # Cleaning the socket by closing it
             s.close()
             sys.exit(0) # Report that the flashing was succesfull
-        except socket.error, msg:
-            print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
+        except socket.error:
+            print ('Failed to create socket.' )
             sys.exit(1)
 
 if __name__ == '__main__':
